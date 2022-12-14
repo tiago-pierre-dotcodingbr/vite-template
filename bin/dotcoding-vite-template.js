2578587a9e48a3b6aab85f8d5f8d152098a62ee6
Tiago Pierre de Mendonça <tiago_pierre@outlook.com>
2022-12-14 12:15:43 -0300

    fix: ajustes gerais

---
+++ bin/dotcoding-vite-template.js
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+    console.log('Você deve fornecer um nome para seu aplicativo.');
+    console.log('Por exemplo :');
+    console.log('    npx dotcoding-reactjs my-app');
+    process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const GIT_REPO =
+    'https://github.com/tiago-pierre-dotcodingbr/vite-template.git';
+
+if (projectName !== '.') {
+    try {
+        fs.mkdirSync(projectPath);
+    } catch (err) {
+        if (err.code === 'EEXIST') {
+            console.log(projectName);
+            console.log(
+                `O arquivo ${projectName} já existe no diretório atual, dê outro nome.`
+            );
+        } else {
+            console.log(err);
+        }
+        process.exit(1);
+    }
+}
+
+async function main() {
+    try {
+        console.log('Baixando arquivos...');
+        execSync(`git clone --depth 1 ${GIT_REPO} ${projectPath}`);
+
+        if (projectName !== '.') {
+            process.chdir(projectPath);
+        }
+
+        console.log('Instalando dependências...');
+        execSync('yarn install');
+
+        console.log('Removendo arquivos inúteis...');
+        execSync('npx rimraf ./.git');
+        execSync('npx rimraf ./bin');
+        execSync('npx rimraf ./.github/workflows');
+        console.log('Preparando projeto...');
+        execSync('yarn prepare');
+
+        console.log('Projeto criado com sucesso, está pronto para usar!');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
